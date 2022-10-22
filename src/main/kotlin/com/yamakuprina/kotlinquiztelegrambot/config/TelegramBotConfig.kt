90f30dd10151c853d339a05b127a5a974cd76e8a
Tagir Faizullin <demoflava@gmail.com>
2022-10-23 00:53:20 +0300

    refactor: add bot config instead of bot class

---
+++ src/main/kotlin/com/yamakuprina/kotlinquiztelegrambot/config/TelegramBotConfig.kt
@@ -0,0 +1,35 @@
+package com.yamakuprina.kotlinquiztelegrambot.config
+
+import com.justai.jaicf.BotEngine
+import com.justai.jaicf.activator.regex.RegexActivator
+import com.justai.jaicf.api.BotApi
+import com.justai.jaicf.channel.telegram.TelegramChannel
+import com.justai.jaicf.model.scenario.Scenario
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class TelegramBotConfig(
+
+    @Value("5770927943:AAEhtHnyBM8azDuKSNZLwXtThUGnclqR24s")
+    val telegramBotToken : String
+) {
+
+    @Bean
+    fun botApi(scenario : Scenario) : BotApi{
+        return BotEngine(
+            scenario = scenario,
+            activators = arrayOf(
+                RegexActivator
+            )
+        )
+    }
+
+    @Bean
+    fun telegramChannel(bot : BotApi) : TelegramChannel{
+        val telegramChannel = TelegramChannel(bot, telegramBotToken)
+        telegramChannel.run()
+        return telegramChannel
+    }
+}
