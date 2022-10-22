0b848341a38f8c88246f4bf7198c6060e056938a
Tagir Faizullin <demoflava@gmail.com>
2022-10-22 09:22:07 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "kotlin-quiz-telegram-bot"
---
+++ src/main/kotlin/com/yamakuprina/kotlinquiztelegrambot/KotlinQuizTelegramBotApplication.kt
@@ -0,0 +1,11 @@
+package com.yamakuprina.kotlinquiztelegrambot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinQuizTelegramBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinQuizTelegramBotApplication>(*args)
+}
