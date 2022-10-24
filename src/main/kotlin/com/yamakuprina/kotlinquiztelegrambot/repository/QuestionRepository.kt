326c0ac15cfe9f32cdd87e326e94d6e5a10ccac7
Tagir Faizullin <demoflava@gmail.com>
2022-10-24 13:14:44 +0300

    feat: add getRandomQuestions function

--- src/main/kotlin/com/yamakuprina/kotlinquiztelegrambot/repository/QuestionRepository.kt
+++ src/main/kotlin/com/yamakuprina/kotlinquiztelegrambot/repository/QuestionRepository.kt
@@ -2,8 +2,12 @@ package com.yamakuprina.kotlinquiztelegrambot.repository
 
 import com.yamakuprina.kotlinquiztelegrambot.model.Question
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface QuestionRepository : JpaRepository<Question, Long> {
+
+    @Query(nativeQuery = true, value = "select * from questions order by random() limit 20")
+    fun getRandomQuestions():List<Question>
 }
