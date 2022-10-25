d2bca7cef13050e351622f786fcff9be3088b80e
Tagir Faizullin <demoflava@gmail.com>
2022-10-25 16:06:32 +0300

    style: QuizScenario

--- src/main/kotlin/com/yamakuprina/kotlinquiztelegrambot/scenario/QuizScenario.kt
+++ src/main/kotlin/com/yamakuprina/kotlinquiztelegrambot/scenario/QuizScenario.kt
@@ -16,8 +16,6 @@ class QuizScenario(
     @Autowired
     val defaultQuestions: List<Question>
 ) : Scenario {
-    //var randomQuestions: MutableList<Question> = mutableListOf()
-    //var wrongAnswers: MutableMap<Question, String> = mutableMapOf()
 
     override val model = createModel {
         state("main") {
@@ -34,60 +32,49 @@ class QuizScenario(
             }
 
             state("quiz") {
-                //var score: Int = 0
 
                 activators {
                     intent("quiz")
                 }
 
                 action {
-                    context.session["randomQuestions"]=questionRepository.getRandomQuestions().toMutableList()
-                    //randomQuestions = questionRepository.getRandomQuestions().toMutableList()
-                    if ((context.session["randomQuestions"] as MutableList<*>).size < 20){
+                    context.session["randomQuestions"] = questionRepository.getRandomQuestions().toMutableList()
+                    if ((context.session["randomQuestions"] as MutableList<*>).size < 20) {
                         context.session["randomQuestions"] = defaultQuestions.shuffled().toMutableList()
                     }
                     if ((context.session["randomQuestions"] as MutableList<*>).size == 0) {
                         reactions.go("/errorFallback")
                     }
                     context.session["wrongAnswers"] = mutableMapOf<Question, String>()
-                    //wrongAnswers = mutableMapOf()
-                    //score = 0
                     context.session["questionNumber"] = 0
                     context.session["score"] = 0
                     reactions.go("/main/quiz/question")
                 }
 
                 state("question") {
-                    //var question: Question? = null
-                    //var options: List<String> = listOf()
-
 
                     activators {
                         regex("/question")
                     }
 
                     action {
-                        context.session["question"] = (context.session["randomQuestions"] as MutableList<*>).removeLast() as Question
-                        //question = (context.session["randomQuestions"] as MutableList<*>).removeLast() as Question?
+                        context.session["question"] =
+                            (context.session["randomQuestions"] as MutableList<*>).removeLast() as Question
                         context.session["options"] = mutableListOf(
                             (context.session["question"] as Question).optionOne,
                             (context.session["question"] as Question).optionTwo,
                             (context.session["question"] as Question).optionThree,
                             (context.session["question"] as Question).correctAnswer
                         ).shuffled()
-//                        options = mutableListOf(
-//                            question!!.optionOne,
-//                            question!!.optionTwo,
-//                            question!!.optionThree,
-//                            question!!.correctAnswer
-//                        ).shuffled()
-                        context.session.compute("questionNumber") { _, v -> v as Int + 1}
-                        val questionMessage: String ="Question "+ (context.session["questionNumber"] as Int) + " of 20"+"\n\n"+
-                            (context.session["question"] as Question).question + "\n" +
-                                "\n1. ${(context.session["options"] as MutableList<*>)[0]}" +
-                                "\n2. ${(context.session["options"] as MutableList<*>)[1]}" +
-                                "\n3. ${(context.session["options"] as MutableList<*>)[2]}" +
-                                "\n4. ${(context.session["options"] as MutableList<*>)[3]}"
+                        context.session.compute("questionNumber") { _, v -> v as Int + 1 }
+
+                        val questionMessage: String =
+                            "Question " + (context.session["questionNumber"] as Int) + " of 20:" + "\n\n" +
+                                    (context.session["question"] as Question).question + "\n" +
+                                    "\n1. ${(context.session["options"] as MutableList<*>)[0]}" +
+                                    "\n2. ${(context.session["options"] as MutableList<*>)[1]}" +
+                                    "\n3. ${(context.session["options"] as MutableList<*>)[2]}" +
+                                    "\n4. ${(context.session["options"] as MutableList<*>)[3]}"
                         reactions.run {
                             say(questionMessage)
                             buttons(
@@ -105,11 +92,10 @@ class QuizScenario(
                         }
                         action {
                             if ((context.session["options"] as MutableList<*>)[0] == (context.session["question"] as Question).correctAnswer) {
-                                context.session.compute("score") { _, v -> v as Int + 1}
-                                //score += 1
+                                context.session.compute("score") { _, v -> v as Int + 1 }
                             } else {
-                                (context.session["wrongAnswers"] as MutableMap<Question, String>)[(context.session["question"] as Question)]=(context.session["options"] as MutableList<*>)[0] as String
-                                //wrongAnswers[question!!] = options[0]
+                                (context.session["wrongAnswers"] as MutableMap<Question, String>)[(context.session["question"] as Question)] =
+                                    (context.session["options"] as MutableList<*>)[0] as String
                             }
                             if ((context.session["randomQuestions"] as MutableList<*>).size == 0) {
                                 reactions.go("/main/quiz/score")
@@ -125,11 +111,10 @@ class QuizScenario(
                         }
                         action {
                             if ((context.session["options"] as MutableList<*>)[1] == (context.session["question"] as Question).correctAnswer) {
-                                context.session.compute("score") { _, v -> v as Int + 1}
-                                //score += 1
+                                context.session.compute("score") { _, v -> v as Int + 1 }
                             } else {
-                                (context.session["wrongAnswers"] as MutableMap<Question, String>)[(context.session["question"] as Question)]=(context.session["options"] as MutableList<*>)[1] as String
-                                //wrongAnswers[question!!] = options[1]
+                                (context.session["wrongAnswers"] as MutableMap<Question, String>)[(context.session["question"] as Question)] =
+                                    (context.session["options"] as MutableList<*>)[1] as String
                             }
                             if ((context.session["randomQuestions"] as MutableList<*>).size == 0) {
                                 reactions.go("/main/quiz/score")
@@ -145,11 +130,10 @@ class QuizScenario(
                         }
                         action {
                             if ((context.session["options"] as MutableList<*>)[2] == (context.session["question"] as Question).correctAnswer) {
-                                context.session.compute("score") { _, v -> v as Int + 1}
-                                //score += 1
+                                context.session.compute("score") { _, v -> v as Int + 1 }
                             } else {
-                                (context.session["wrongAnswers"] as MutableMap<Question, String>)[(context.session["question"] as Question)]=(context.session["options"] as MutableList<*>)[2] as String
-                                //wrongAnswers[question!!] = options[2]
+                                (context.session["wrongAnswers"] as MutableMap<Question, String>)[(context.session["question"] as Question)] =
+                                    (context.session["options"] as MutableList<*>)[2] as String
                             }
                             if ((context.session["randomQuestions"] as MutableList<*>).size == 0) {
                                 reactions.go("/main/quiz/score")
@@ -165,11 +149,10 @@ class QuizScenario(
                         }
                         action {
                             if ((context.session["options"] as MutableList<*>)[3] == (context.session["question"] as Question).correctAnswer) {
-                                context.session.compute("score") { _, v -> v as Int + 1}
-                                //score += 1
+                                context.session.compute("score") { _, v -> v as Int + 1 }
                             } else {
-                                (context.session["wrongAnswers"] as MutableMap<Question, String>)[(context.session["question"] as Question)]=(context.session["options"] as MutableList<*>)[3] as String
-                                //wrongAnswers[question!!] = options[3]
+                                (context.session["wrongAnswers"] as MutableMap<Question, String>)[(context.session["question"] as Question)] =
+                                    (context.session["options"] as MutableList<*>)[3] as String
                             }
                             if ((context.session["randomQuestions"] as MutableList<*>).size == 0) {
                                 reactions.go("/main/quiz/score")
