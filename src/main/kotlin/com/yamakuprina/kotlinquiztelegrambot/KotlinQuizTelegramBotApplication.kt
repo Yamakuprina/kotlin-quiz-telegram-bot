df8f084a88c0c0ba1749a9362654fc134d3f2f17
Tagir Faizullin <demoflava@gmail.com>
2022-11-03 14:43:23 +0300

    fix: app config scan

--- src/main/kotlin/com/yamakuprina/kotlinquiztelegrambot/KotlinQuizTelegramBotApplication.kt
+++ src/main/kotlin/com/yamakuprina/kotlinquiztelegrambot/KotlinQuizTelegramBotApplication.kt
@@ -1,9 +1,13 @@
 package com.yamakuprina.kotlinquiztelegrambot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan("com.yamakuprina.kotlinquiztelegrambot.config")
 class KotlinQuizTelegramBotApplication
 
 fun main(args: Array<String>) {
